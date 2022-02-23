4366b42a92888d256a9f639c65a9f4670d9a16f3
kingzy <kingzyv14@gmail.com>
2022-02-23 15:14:57 -0800

    config deployment

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
